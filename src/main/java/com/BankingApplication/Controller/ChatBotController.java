a922670616739fdee571db95d708407b887f27cd
Minh20Duc04 <nguyenminhduc02042004@gmail.com>
2025-04-24 13:00:47 +0700

    Change chatbot GET method to the correct method.

--- src/main/java/com/BankingApplication/Controller/ChatBotController.java
+++ src/main/java/com/BankingApplication/Controller/ChatBotController.java
@@ -4,10 +4,7 @@ import com.BankingApplication.Dto.ChatBotRequest;
 import com.BankingApplication.Dto.ChatBotResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -26,7 +23,7 @@ public class ChatBotController {
     private RestTemplate template;
 
 
-    @GetMapping("/chat")
+    @PostMapping("/chat")
     public String chat(@RequestParam("prompt") String prompt){
         ChatBotRequest request = new ChatBotRequest(model, prompt);
         ChatBotResponse chatBotResponse = template.postForObject(apiURL, request, ChatBotResponse.class);
